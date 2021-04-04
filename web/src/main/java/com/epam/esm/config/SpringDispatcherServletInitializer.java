f903cd99681f274ec30b9242f6cecabc414c19e0
YeeeeahRight <yeeeeeah.right@gmail.com>
2021-04-04 19:39:59 +0300

    add initial spring config

---
+++ web/src/main/java/com/epam/esm/config/SpringDispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package com.epam.esm.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//instead of WebAppInitializer
+public class SpringDispatcherServletInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}

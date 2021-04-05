34959bc859b16dc4875ce12cd3bda404ee550f00
YeeeeahRight <yeeeeeah.right@gmail.com>
2021-04-06 01:17:53 +0300

    Add CRD operations in TagController

--- web/src/main/java/com/epam/esm/controller/TagController.java
+++ web/src/main/java/com/epam/esm/controller/TagController.java
@@ -1,28 +1,48 @@
 package com.epam.esm.controller;
 
+import com.epam.esm.entity.Tag;
+import com.epam.esm.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/tags")
 public class TagController {
+    private final TagService tagService;
+
+    @Autowired
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
 
     @PostMapping
-    public void create() {
-        System.out.println("CREATE TAG...");
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Tag tag, HttpServletResponse response) {
+        long id = tagService.create(tag);
+        response.addHeader("Location:", "/tags/" + id);
     }
 
     @GetMapping
-    public void getAll() {
-        System.out.println("GET ALL TAGS...");
+    @ResponseStatus(HttpStatus.OK)
+    public Set<Tag> getAll() {
+        return tagService.getAll();
     }
 
-    @GetMapping("/{id}")
-    public void getById(@PathVariable("id") int id) {
-        System.out.println("GET " + id + " TAG...");
+    @GetMapping(value = "/{id}", produces = "application/json")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public Tag getById(@PathVariable("id") int id) {
+        return tagService.getById(id);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable("id") int id) {
-        System.out.println("DELETE " + id + " TAG");
+        tagService.deleteById(id);
     }
 }

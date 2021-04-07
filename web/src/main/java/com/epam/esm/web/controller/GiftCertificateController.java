51413b51c92b60031e5d18c7886089b06c13afa1
YeeeeahRight <yeeeeeah.right@gmail.com>
2021-04-07 03:30:36 +0300

    Add gift certificate controller

---
+++ web/src/main/java/com/epam/esm/web/controller/GiftCertificateController.java
@@ -0,0 +1,60 @@
+package com.epam.esm.web.controller;
+
+import com.epam.esm.persistence.entity.GiftCertificate;
+import com.epam.esm.service.service.GiftCertificateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@RequestMapping("/certificates")
+public class GiftCertificateController {
+    private final GiftCertificateService giftCertificateService;
+
+    @Autowired
+    public GiftCertificateController(GiftCertificateService giftCertificateService) {
+        this.giftCertificateService = giftCertificateService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody GiftCertificate giftCertificate, HttpServletResponse response) {
+        long id = giftCertificateService.create(giftCertificate);
+        response.addHeader("Location:", "/certificates/" + id);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public GiftCertificate getById(@PathVariable("id") long id) {
+        return giftCertificateService.getById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public GiftCertificate updateById(@PathVariable("id") long id,
+                                      @RequestBody GiftCertificate giftCertificate) {
+        return giftCertificateService.updateById(id, giftCertificate);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<GiftCertificate> getAll() {
+        return giftCertificateService.getAll();
+    }
+
+    @GetMapping("/search")
+    @ResponseStatus(HttpStatus.OK)
+    public List<GiftCertificate> search(@RequestParam(required = false, defaultValue = "") String tagName,
+                       @RequestParam(required = false, defaultValue = "") String partInfo,
+                       @RequestParam(required = false) List<String> sortTypes) {
+        return giftCertificateService.search(tagName, partInfo, sortTypes);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable("id") long id) {
+        giftCertificateService.deleteById(id);
+    }
+}
